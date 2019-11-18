2a6e1ab07d6c016f73ce169abd39973ccc77deff
Logan May <loma@sas.upenn.edu>
2019-11-17 20:48:36 -0500

    add derived type classes to Ast.hs

--- src/Ast.hs
+++ src/Ast.hs
@@ -11,21 +11,25 @@ data Ty =
  | TInt
 -- | TRef of rty
 -- | TNullRef of rty
+  deriving (Show, Eq)
 
 data Rty =
 -- | RString
 -- | RStruct of id
 -- | RArray of ty
   RFun [Ty] Retty
+  deriving (Show, Eq)
 
 data Retty =
     RetVoid
   | RetVal Ty
+  deriving (Show, Eq)
 
 data Unop =
    Neg
  | Lognot
  | Bitnot
+  deriving (Show, Eq, Enum)
 
 data Binop =
    Add
@@ -45,6 +49,7 @@ data Binop =
  | Shl
  | Shr
  | Sar
+  deriving (Show, Eq, Enum)
 
 -- add binops for floats
 
@@ -63,10 +68,11 @@ data Exp =
  | Call (Node Exp) [Node Exp]
  | Bop Binop (Node Exp) (Node Exp)
  | Uop Unop (Node Exp)
+ deriving (Show, Eq)
 
 -- type cfield = id * exp node
 
-data Vdecl = VDecl Id (Node Exp)
+data Vdecl = VDecl Id (Node Exp) deriving (Show, Eq)
 
 data Stmt = 
    Assn (Node Exp) (Node Exp)
@@ -77,12 +83,13 @@ data Stmt =
 -- | Cast of rty * id * exp node * stmt node list * stmt node list
  | For [Vdecl] (Maybe (Node Exp)) (Maybe (Node Stmt)) [Node Stmt]
  | While (Node Exp) [Node Stmt]
+ deriving (Show, Eq)
 
 type Block = [Node Stmt]
 
 -- type gdecl = { name : id; init : exp node }
 
-data Fdecl = Fdecl { retty :: Retty, fname :: Id, args :: [(Ty, Id)], body :: Block }
+data Fdecl = Fdecl { retty :: Retty, fname :: Id, args :: [(Ty, Id)], body :: Block } deriving (Show, Eq)
 
 -- data Field = Field { fieldName :: Id, ftyp :: Ty }
 
@@ -92,6 +99,7 @@ newtype Decl =
 -- | Gvdecl of gdecl node
    Gfdecl (Node Fdecl)
 -- | Gtdecl (Node Tdecl)
+  deriving (Show, Eq)
 
 type Prog = [Decl]
 
