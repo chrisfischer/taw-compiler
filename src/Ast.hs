7b6ca5d18e059a994091ab4dede0cc082181550e
Chris Fischer <chris.fischer36@gmail.com>
2019-11-17 22:15:20 -0500

    Removed comments

--- src/Ast.hs
+++ src/Ast.hs
@@ -9,96 +9,74 @@ type Id = String
 data Ty =
    TBool
  | TInt
--- | TRef of rty
--- | TNullRef of rty
   deriving (Show, Eq)
 
 data Rty =
--- | RString
--- | RStruct of id
--- | RArray of ty
   RFun [Ty] Retty
   deriving (Show, Eq)
 
 data Retty =
-    RetVoid
-  | RetVal Ty
+  RetVal Ty
   deriving (Show, Eq)
 
 data Unop =
    Neg
  | Lognot
- | Bitnot
   deriving (Show, Eq, Enum)
 
 data Binop =
-   Add
- | Sub
- | Mul
- | Div
- | Eq
- | Neq
- | Lt
- | Lte
- | Gt
- | Gte
- | And
- | Or
- | IAnd
- | IOr
- | Shl
- | Shr
- | Sar
+    Add
+  | Sub
+  | Mul
+  | Div
+  | Eq
+  | Neq
+  | Lt
+  | Lte
+  | Gt
+  | Gte
+  | And
+  | Or
+  | IAnd
+  | IOr
+  | Shl
+  | Shr
+  | Sar
+  | Mod
   deriving (Show, Eq, Enum)
 
--- add binops for floats
-
 data Exp =
--- | CNull of rty
-   CBool Bool
- | CInt Int64
--- | CStr of string
- | Id Id
--- | CArr of ty * exp node list
--- | NewArr of ty * exp node * id * exp node
--- | Index of exp node * exp node
--- | Length of exp node
--- | CStruct of id * (id * exp node) list
--- | Proj of exp node * id
- | Call (Node Exp) [Node Exp]
- | Bop Binop (Node Exp) (Node Exp)
- | Uop Unop (Node Exp)
- deriving (Show, Eq)
-
--- type cfield = id * exp node
+    CBool Bool
+  | CInt Int64
+  | Id Id
+  | Call (Node Exp) [Node Exp]
+  | Bop Binop (Node Exp) (Node Exp)
+  | Uop Unop (Node Exp)
+  deriving (Show, Eq)
 
 data Vdecl = VDecl Id (Node Exp) deriving (Show, Eq)
 
-data Stmt = 
-   Assn (Node Exp) (Node Exp)
- | Decl Vdecl
- | Ret (Maybe (Node Exp))
- | SCall (Node Exp) [Node Exp]
- | If (Node Exp) [Node Stmt] [Node Stmt]
--- | Cast of rty * id * exp node * stmt node list * stmt node list
- | For [Vdecl] (Maybe (Node Exp)) (Maybe (Node Stmt)) [Node Stmt]
- | While (Node Exp) [Node Stmt]
- deriving (Show, Eq)
+data Stmt =
+    Assn (Node Exp) (Node Exp)
+  | Decl Vdecl
+  | Ret (Maybe (Node Exp))
+  | SCall (Node Exp) [Node Exp]
+  | If (Node Exp) [Node Stmt] [Node Stmt]
+  | For [Vdecl] (Maybe (Node Exp)) (Maybe (Node Stmt)) [Node Stmt]
+  | While (Node Exp) [Node Stmt]
+  deriving (Show, Eq)
 
 type Block = [Node Stmt]
 
--- type gdecl = { name : id; init : exp node }
-
-data Fdecl = Fdecl { retty :: Retty, fname :: Id, args :: [(Ty, Id)], body :: Block } deriving (Show, Eq)
-
--- data Field = Field { fieldName :: Id, ftyp :: Ty }
-
--- type Tdecl = [(Id, Field)]
+data Fdecl = Fdecl {
+    retty :: Retty
+  , fname :: Id
+  , args :: [(Ty, Id)]
+  , body :: Block
+  } deriving (Show, Eq)
 
 newtype Decl =
--- | Gvdecl of gdecl node
-   Gfdecl (Node Fdecl)
--- | Gtdecl (Node Tdecl)
+  Gfdecl (Node Fdecl)
   deriving (Show, Eq)
 
 type Prog = [Decl]
