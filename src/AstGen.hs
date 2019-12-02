780a86b66f51df166829006228b2a4e5950117bc
Logan May <loma@eniac.seas.upenn.edu>
2019-12-02 00:41:40 -0500

    WIP: generating correct programs - expressions are correct but call not implemented, added context data type to support correct referencing and typing.

--- src/AstGen.hs
+++ src/AstGen.hs
@@ -4,11 +4,47 @@ import Ast
 import Control.Monad
 import Test.QuickCheck
 import Test.QuickCheck.Gen
+import Data.Map as Map
 
 --------------------------------------
--- CONTEXTS --------------------------
+-- UTILS - ---------------------------
 --------------------------------------
--- TODO
+
+nol x = noLoc <$> x
+
+
+--------------------------------------
+-- CONTEXT ---------------------------
+--------------------------------------
+
+data Ctxt = Ctxt {vars :: Map String [String], -- lookup vars by ty string
+                  funs :: Map String [String], -- lookup functions by retty string
+                  funTypes :: Map String Rty , -- lookup functions Rtys by id
+                  currentFun :: String       }
+
+-- helpers
+idsOfType :: String -> Ctxt -> [Exp]
+idsOfType t c =
+  case Map.lookup t (vars c) of
+    Nothing   -> []
+    Just vars -> Ast.Id <$> vars
+
+funsOfType :: String -> Ctxt -> [String]
+funsOfType t c =
+  case Map.lookup t (funs c) of
+    Nothing   -> []
+    Just funs -> funs
+
+currentRetty :: Ctxt -> Retty
+currentRetty c = let RFun _ retty = funTypes c ! (currentFun c) in retty
+
+-- test contexts
+myC :: Ctxt
+myC = Ctxt (insert "TBool" ["bool1", "bool2"] 
+             (insert "TInt" ["int1", "int2"] empty))
+           (insert "TBool" ["fBool"] empty)
+           (insert "fBool" (RFun [TBool, TBool] (RetVal TBool)) empty)
+           ""
 
 --------------------------------------
 -- OPERATOR GENERATORS ---------------
@@ -30,55 +66,85 @@ genOrdCompBinop = elements [Ast.Lt, Ast.Lte, Ast.Gt, Ast.Gte]
 genEqCompBinop :: Gen Binop
 genEqCompBinop = elements [Ast.Eq, Ast.Neq]
 
+
 --------------------------------------
 -- EXPRESSION GENERATORS -------------
 --------------------------------------
-genExp :: Gen Exp
-genExp = sized genExp'
+genMaybeNodeExp :: Ctxt -> Gen (Maybe (Node Exp))
+genMaybeNodeExp c =
+  oneof [Just <$> genNodeExp c,
+         return Nothing       ]
 
-genExp' :: Int -> Gen Exp
-genExp' n =
-  oneof [genIntExp'  n,
-         genBoolExp' n]
---         genCallExp   ,
---         genIdExp     ]
+genNodeExp :: Ctxt -> Gen (Node Exp)
+genNodeExp c = noLoc <$> (genExp c)
 
-genIntExp :: Gen Exp
-genIntExp = sized genIntExp'
+genExp :: Ctxt -> Gen Exp
+genExp c = sized (genExp' c)
 
-genIntExp' :: Int -> Gen Exp
-genIntExp' 0 = CInt <$> arbitrary
-genIntExp' n | n > 0 =
-  oneof [genIntExp' 0,
+genExp' :: Ctxt -> Int -> Gen Exp
+genExp' c n =
+  oneof [genIntExp'  c n,
+         genBoolExp' c n]
+
+genIntExp :: Ctxt -> Gen Exp
+genIntExp c = sized (genIntExp' c)
+
+genIntExp' :: Ctxt -> Int -> Gen Exp
+genIntExp' c 0 =
+  let arbGen   = CInt <$> arbitrary in
+  let idGens   = return <$> (idsOfType "TInt" c) in
+  oneof $ [arbGen] ++ idGens -- TODO: fix this so it's evenly balanced
+genIntExp' c n | n > 0 =
+  oneof [genIntExp' c 0,
          liftM3 Bop genIntBinop subExp subExp]
-  where subExp = noLoc <$> (genIntExp' (n `div` 2))
+  where subExp = noLoc <$> (genIntExp' c (n `div` 2))
 
-genBoolExp :: Gen Exp
-genBoolExp = sized genBoolExp'
+genBoolExp :: Ctxt -> Gen Exp
+genBoolExp c = sized (genBoolExp' c)
 
-genBoolExp' :: Int -> Gen Exp
-genBoolExp' 0 = CBool <$> arbitrary
-genBoolExp' n | n > 0 =
-  oneof [genBoolExp' 0                            ,
+genBoolExp' :: Ctxt -> Int -> Gen Exp
+genBoolExp' c 0 =
+  let arbGen = CBool <$> arbitrary in
+  let idGens = return <$> (idsOfType "TBool" c) in
+  oneof $ [arbGen] ++ idGens
+genBoolExp' c n | n > 0 =
+  oneof [genBoolExp' c 0                          ,
          liftM2 Uop genBoolUnop boolExp           ,
          liftM3 Bop genBoolBinop boolExp boolExp  ,
          liftM3 Bop genOrdCompBinop intExp intExp , -- TODO: maybe not just ints
          liftM3 Bop genEqCompBinop intExp intExp  ,
          liftM3 Bop genEqCompBinop boolExp boolExp] -- TODO: add other comps
-  where boolExp = noLoc <$> (genBoolExp' n')
-        intExp  = noLoc <$> (genIntExp'  n')
+  where boolExp = noLoc <$> (genBoolExp' c n')
+        intExp  = noLoc <$> (genIntExp'  c n')
         n'      = n `div` 2
+
+
+genIdExpOpt :: Ctxt -> Maybe (Gen Exp)
+genIdExpOpt c =
+  let ids = concatMap (\k -> vars c ! k) (keys (vars c)) in
+  case ids of
+    []    -> Nothing
+    x:xs  -> Just $ Ast.Id <$> (elements ids)
+
+genExpOfType :: Ctxt -> Ty -> Gen Exp
+genExpOfType c ty =
+  case ty of
+    TInt     -> genIntExp c
+    TBool    -> genBoolExp c
+    TRef rty -> error "not generating expressions for function types"
+
+-- WIP: getting the call expressions in there
+idsForTys :: Ctxt -> [Ty] -> [Maybe (Gen Exp)]
+idsForTys c tys =
+  do ty <- tys
+     case Map.lookup (show ty) (vars c) of
+       Nothing  -> return Nothing
+       Just ids -> return $ Just $ elements $ Ast.Id <$> ids
+
 
 --------------------------------------
 -- TYPE GENERATORS -------------------
 --------------------------------------
-
-
--------------------------------------
--- OLD ------------------------------
--------------------------------------
-genFromEnum :: Enum a => a -> Gen a
-genFromEnum e = elements $ enumFrom e
 
 genTy :: Gen Ty
 genTy = sized genTy'
@@ -94,52 +160,61 @@ genRty :: Gen Rty
 genRty = sized genRty'
 
 genRty' :: Int -> Gen Rty
-genRty' n = liftM2 RFun (listOf $ genTy' n) (genRetty' n)
+genRty' n = liftM2 RFun (listOf $ genTy' n) genRetty
 
 genRetty :: Gen Retty
-genRetty = sized genRetty'
+genRetty = 
+  oneof $ return <$> [RetVoid     ,
+                      RetVal TInt ,
+                      RetVal TBool]
 
-genRetty' :: Int -> Gen Retty
-genRetty' n = 
-  frequency [(4, RetVal <$> (genTy' n)), 
-             (1, return RetVoid       )] 
+-- | old version could return function types, but didn't make sense
+-- genRetty' :: Int -> Gen Retty
+-- genRetty' n = 
+--  frequency [(4, RetVal <$> (genTy' n)), 
+--             (1, return RetVoid       )] 
 
--- genValueTy :: Gen ValueTy
--- genValueTy =
---  oneof [VBool <$> arbitrary, 
---         VInt  <$> arbitrary,
---         VFun  <$> arbitrary]
 
-genMaybeNodeExp :: Gen (Maybe (Node Exp))
-genMaybeNodeExp =
-  oneof [Just <$> genNodeExp,
-         return Nothing     ]
+--------------------------------------
+-- STATEMENT GENERATORS  -------------
+--------------------------------------
 
-genNodeExp :: Gen (Node Exp)
-genNodeExp = noLoc <$> genExp
+genMaybeNodeStmt :: Ctxt -> Gen (Maybe (Node Stmt))
+genMaybeNodeStmt c =
+  oneof [Just <$> genNodeStmt c,
+         return Nothing        ]
 
-{-
-genExp :: Gen Exp
-genExp = sized genExp'
+genNodeStmt :: Ctxt -> Gen (Node Stmt)
+genNodeStmt c = sized (genNodeStmt' c)
 
-genExp' :: Int -> Gen Exp
-genExp' 0 = 
-  oneof [CBool <$> arbitrary,
-         CInt  <$> arbitrary,
-         Id    <$> arbitrary]
-genExp' n | n > 0 =
-  oneof [genBoolExp' n                        ,
-         fmap   CInt  arbitrary               ,
-         fmap   Id    arbitrary               ,
-         liftM3 Bop   genBinop subExp subExp  ,
-         liftM2 Uop   genUnop  subExp         ,
-         liftM2 Call  subExp   (listOf subExp)]
-    where subExp = noLoc <$> genExp' (n `div` 2)
+genNodeStmt' :: Ctxt -> Int -> Gen (Node Stmt)
+genNodeStmt' c n = noLoc <$> (genStmt' c n)
 
--}
-genVdecl :: Gen Vdecl
-genVdecl = liftM2 Vdecl arbitrary genNodeExp
+genStmt :: Ctxt -> Gen Stmt
+genStmt c = sized (genStmt' c)
 
+genStmt':: Ctxt -> Int -> Gen Stmt
+genStmt' c 0 =
+  let vdecl = [Decl <$> (genVdecl c)] in
+  let assn = case genIdExpOpt c of
+               Nothing    -> []
+               Just idGen -> [liftM2 Assn (nol idGen) (genNodeExp c)] in
+  let ret = case currentRetty c of
+              RetVoid   -> []
+              RetVal ty -> [fmap Ret (nol $ genExpOfType c ty)] in
+  oneof $ vdecl ++ assn ++ ret
+
+{--
+ - So, a few things:
+ - the left exp on Assn needs to update the context
+ - genVdecl needs to create a new valid id and update the context
+ - Ret needs to know the return type and create the proper expression
+--}
+
+genVdecl :: Ctxt -> Gen Vdecl
+genVdecl c = liftM2 Vdecl arbitrary (genNodeExp c)
+
+{--
 genBlock :: Gen Block
 genBlock = sized genBlock'
 
@@ -149,26 +224,12 @@ genBlock' n | n > 0 =
   vectorOf n' (genNodeStmt' n')
   where n' = n `div` 2
 
-genMaybeNodeStmt :: Gen (Maybe (Node Stmt))
-genMaybeNodeStmt =
-  oneof [Just <$> genNodeStmt,
-         return Nothing      ]
-
-genNodeStmt :: Gen (Node Stmt)
-genNodeStmt = sized genNodeStmt'
-
-genNodeStmt' :: Int -> Gen (Node Stmt)
-genNodeStmt' n = noLoc <$> (genStmt' n)
-
-genStmt :: Gen Stmt
-genStmt = sized genStmt'
-
-genStmt' :: Int -> Gen Stmt
-genStmt' 0 = 
+genStmt' :: Vars -> Int -> Gen Stmt
+genStmt' vars 0 = 
   oneof [liftM2 Assn genNodeExp genNodeExp            ,
          fmap   Decl genVdecl                         ,
          fmap   Ret  genNodeExp                       , 
-         liftM3 If   genNodeExp genBlock genBlock     ,
+         liftM3 If   genNodeEx genBlock genBlock      ,
          liftM4 For  (listOf genVdecl) genMaybeNodeExp
                      genMaybeNodeStmt  genBlock       ]
 
@@ -181,14 +242,10 @@ genArg = liftM2 (,) genTy arbitrary
 genFdecl :: Gen Fdecl
 genFdecl = liftM4 Fdecl genRetty arbitrary genArgs genBlock
 
-genFext :: Gen Fext
-genFext = liftM3 Fext genRetty arbitrary genArgs
-
 genDecl :: Gen Decl
 genDecl = Gfdecl . noLoc <$> genFdecl
---  oneof [Gfdecl . noLoc <$> genFdecl,
---         Gfext  . noLoc <$> genFext ]
 
 genProg :: Gen Prog
 genProg = listOf genDecl
 
+--}
